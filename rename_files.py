383d1c12c9aaeabe92af2a96f421dcaf4a937f5d
kboretska <129294302+kboretska@users.noreply.github.com>
2025-02-12 11:50:44 +0200

    Update rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -3,17 +3,17 @@ import glob
 import sys
 
 directory = sys.argv[1]
-file_type = sys.argv[2]
+file_types = sys.argv[2].split(",")
 slice_index = int(sys.argv[3])
 
 os.chdir(directory)
-
-for file in glob.glob(f"*.{file_type}"):
-    file_name, extension = os.path.splitext(file)
-    new_file_name = file_name[:-slice_index] + extension
-    try:
-        os.rename(file, new_file_name)
-    except OSError as e:
-        print(e)
-    else:
-        print(f"Renamed {file} to {new_file_name}")
+for file_type in file_types:
+        for file in glob.glob(f"*.{file_type}"):
+                file_name, extension = os.path.splitext(file)
+                new_file_name = file_name[:-slice_index] + extension
+                try:
+                        os.rename(file, new_file_name)
+                except OSError as e:
+                        print(e)
+                else:
+                        print(f"Renamed {file} to {new_file_name}")
